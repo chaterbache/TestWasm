aa50bea2689b80222b3ac303e1fe38ddb08e4b4f
chaterbache <chaterbache@gmail.com>
2022-02-05 16:49:14 +0100

    Adding sum for arrays

--- index.js
+++ index.js
@@ -8,8 +8,16 @@ let importObject = {
 
 WebAssembly.instantiateStreaming(fetch('main.wasm'), importObject)
 .then(results => {
-  // Do something with the results!
-  console.log(results.instance.exports.add(1,1))
-  console.log(results.instance.exports.sub(1,2))
-  console.log(results.instance.exports.mul(1,2))
+  
+  const { add, sub, mul, sum, memory } = results.instance.exports
+
+  console.log(add(1,1))
+  console.log(sub(1,2))
+  console.log(mul(1,2))
+  b = [1, 2, 4, 5, 1999,4444444];
+
+  const a = new Int32Array(memory.buffer, 0, b.length)
+  a.set(b);
+
+  console.log(sum(a.byteOffset, a.length))
 });
--- main.c
+++ main.c
@@ -17,13 +17,23 @@ int mul(int a, int b)
 	return a * b;
 }
 EMSCRIPTEN_KEEPALIVE
-int div(int a, int b)
+int divise(int a, int b)
 {
-	return a / b;
+	return  a / b;
 }
 
 EMSCRIPTEN_KEEPALIVE
 int mod(int a, int b)
 {
 	return a % b;
+}
+
+EMSCRIPTEN_KEEPALIVE
+int sum(int *a, int l)
+{
+	int s = 0;
+	for(int i=0; i < l; ++i){
+		s += a[i];
+	}
+	return s;
 }
